584b49948229399bb6437e737e2fd55766ef1d90
Dave Cascino <david.cascino@gmail.com>
2012-01-22 16:27:08 -0500

    added stub ios project

---
+++ tumbleweed/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  tumbleweed
+//
+//  Created by David Cascino on 1/22/12.
+//  Copyright (c) 2012 AI Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    IBOutlet UIScrollView *scrollView;
+    
+}
+
+@property (nonatomic, retain) UIScrollView *scrollView;
+
+@end
