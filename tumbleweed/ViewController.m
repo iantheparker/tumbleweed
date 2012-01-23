901ccfa9bdd4acbb8a81fa6ab3ec4478c8fbd7b8
Dave Cascino <david.cascino@gmail.com>
2012-01-22 21:03:39 -0500

    prevent portrait orientation

--- tumbleweed/ViewController.m
+++ tumbleweed/ViewController.m
@@ -74,7 +74,8 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    return (interfaceOrientation == UIInterfaceOrientationLandscapeLeft 
+            || interfaceOrientation == UIInterfaceOrientationLandscapeRight);
 }
 
 @end
