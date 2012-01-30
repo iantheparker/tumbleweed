696606ee09c82de0f636a87b30b3fe936cd500bc
ian parker <ianrparker@gmail.com>
2012-01-30 15:33:05 -0500

    printed out names of venues

--- tumbleweed/SceneController.m
+++ tumbleweed/SceneController.m
@@ -51,7 +51,6 @@
     
     NSURL *url = [NSURL URLWithString:urlString];
     NSError *err;
-    NSLog(@"urlstring %@", urlString);
     
     // fetch the data (TODO async)
     NSString *venues = [NSString stringWithContentsOfURL:url 
@@ -62,26 +61,40 @@
     NSDictionary *venuesDict = [NSDictionary dictionaryWithJSONString:venues 
                                                                 error:&err];
 
+
     
 
     //NSLog(@"venues %@", venuesDict);
-    NSLog(@"venues keys%@", venuesDict.allKeys);
+    //NSLog(@"venues keys%@", venuesDict.allKeys);
     //NSLog(@"venues values%@", venuesDict.allValues);
-    NSLog(@"venues total %d", venuesDict.count);
-    NSLog(@"value for notifcations key%@", [venuesDict valueForKey:@"notifications"]);
-    NSLog(@"value for meta key%@", [venuesDict valueForKey:@"meta"]);
+    //NSLog(@"venues total %d", venuesDict.count);
+    //NSLog(@"value for notifcations key%@", [venuesDict valueForKey:@"notifications"]);
+    //NSLog(@"value for meta key%@", [venuesDict valueForKey:@"meta"]);
     //NSLog(@"value for response key%@", [venuesDict valueForKey:@"response"]);
-    NSLog(@"value for response key%@", [[venuesDict valueForKey:@"response"] allKeys]);
-    NSDictionary *response = [[venuesDict valueForKey:@"response"] objectForKey:@"groups"];
-    //NSLog(@"value for response values%@", [[venuesDict valueForKey:@"response"] allValues]);
-    NSLog(@"response = %@", response);
+    //NSLog(@"value for response key%@", [[venuesDict valueForKey:@"response"] allKeys]);
+    NSDictionary *response = [venuesDict objectForKey:@"response"];
+    NSArray *groups = [response objectForKey:@"groups"];
+    NSDictionary *group1 = [groups objectAtIndex:0];
+    NSArray *items = [group1 objectForKey:@"items"];
+    for (int i = 0; i < [items count]; i++) {
+        NSDictionary *ven = [items objectAtIndex:i];
+        NSString *name = [ven objectForKey:@"name"];
+        NSLog(@"item %d is called %@", i, name);
+    }
     
-
+    //NSArray *items = [[[venuesDict objectForKey:@"response"] objectForKey:@"groups"] objectForKey:@"items"];
+    
+    
+    
+    //NSLog(@"value for response values%@", [[venuesDict valueForKey:@"response"] allValues]);
+    //NSLog(@"response = %d", items.count);
+    
+    NSLog(@"%d", [items count]);
+    
     // loop through each venue and add a label to the venue
     
-    
     //NSLog(@"venues %@", venuesDict);
-    NSLog(@"venues %@", venues);
+    //NSLog(@"venues %@", venues);
 }
 
 - (void)viewDidUnload
