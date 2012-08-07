6edae227ef2617a10e04f70da390f0bd6178476c
iantheparker <ianrparker@gmail.com>
2012-08-07 12:39:24 -0700

    p2

--- tumbleweed/AppDelegate.m
+++ tumbleweed/AppDelegate.m
@@ -147,6 +147,9 @@ void uncaughtExceptionHandler(NSException *exception) {
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
+    
+    [UIApplication sharedApplication].applicationIconBadgeNumber=0;
+
 }
 
 
