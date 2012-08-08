d293343090b87c88efd055c596c7dd195d32681a
iantheparker <ianrparker@gmail.com>
2012-08-07 17:51:15 -0700

    build configuration settings
    now determines if your in dev or prod mode and sets you up with the
    proper server and client ids

---
+++ tumbleweed/Environment.h
@@ -0,0 +1,13 @@
+//
+//  Environment.h
+//  tumbleweed
+//
+//  Created by Ian Parker on 8/7/12.
+//  Copyright (c) 2012 AI Capital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Environment : NSObject
+
+@end
---
+++ tumbleweed/Environment.m
@@ -0,0 +1,13 @@
+//
+//  Environment.m
+//  tumbleweed
+//
+//  Created by Ian Parker on 8/7/12.
+//  Copyright (c) 2012 AI Capital. All rights reserved.
+//
+
+#import "Environment.h"
+
+@implementation Environment
+
+@end
