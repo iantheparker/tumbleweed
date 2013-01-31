b0211954425fe4270aa6837233b0e7a6502631ef
iantheparker <ianrparker@gmail.com>
2013-01-31 04:02:51 -0500

    almost finished refactoring sceneVC

---
+++ tumbleweed/BonusWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  BonusWebViewController.h
+//  tumbleweed
+//
+//  Created by Ian Parker on 1/30/13.
+//  Copyright (c) 2013 AI Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BonusWebViewController : UIViewController
+
+@end
---
+++ tumbleweed/BonusWebViewController.m
@@ -0,0 +1,38 @@
+//
+//  BonusWebViewController.m
+//  tumbleweed
+//
+//  Created by Ian Parker on 1/30/13.
+//  Copyright (c) 2013 AI Capital. All rights reserved.
+//
+
+#import "BonusWebViewController.h"
+
+@interface BonusWebViewController ()
+
+@end
+
+@implementation BonusWebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
